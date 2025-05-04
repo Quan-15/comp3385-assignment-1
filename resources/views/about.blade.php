648d12bb31018f849b8111dd23569593b4ee067f
Quan-15 <dd15031234@gmail.com>
2025-05-03 22:31:44 -0400

    Styled about page and added image

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,3 +4,4 @@
     <h2>Your Name</h2>
     <p>Write a short paragraph about yourself.</p>
 @endsection
+<img src="{{ url("C:\Users\dd150\OneDrive\Pictures\Wallpapers\20200209_101436.jpg") }}" alt="Your alternate description" />
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>COMP3385 - Lab 1</title>
+    <link rel="stylesheet" href="{{ url('/css/app.css') }}" />
+
 </head>
 <body>
     <header>
